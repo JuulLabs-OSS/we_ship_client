9c469e7ddddb85e4dc072c15a1f44f70e1c95336
lukaszkomursofomo <lukasz.komur@sofomo.com>
2022-03-01 14:01:48 +0100

    Add GetTracking parameter to change timeout

--- lib/we_ship_client/client.rb
+++ lib/we_ship_client/client.rb
@@ -5,17 +5,22 @@ require 'loogi_http/connection'
 
 module WeShipClient
   class Client
+    DEFAULT_TIMEOUT = 90
+
     attr_reader :http_client, :logger
 
     # @param http_client [LoogiHttp::Connection]
     # @param logger [Logger]
-    def initialize(http_client: LoogiHttp::Connection, logger: LoogiHttp.logger)
+    # @param timeout [Integer,nil] The request timeout
+    def initialize(http_client: LoogiHttp::Connection, logger: LoogiHttp.logger, timeout: nil)
+      timeout ||= DEFAULT_TIMEOUT
+
       @http_client = http_client.new(
         Faraday::Connection.new do |connection|
           connection.request :json
           connection.adapter Faraday.default_adapter
-          connection.options[:open_timeout] = 90
-          connection.options[:timeout] = 90
+          connection.options[:open_timeout] = timeout
+          connection.options[:timeout] = timeout
         end
       )
       @logger = logger
